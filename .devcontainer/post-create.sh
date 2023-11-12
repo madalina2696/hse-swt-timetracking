2f577779025c9ca657a8eeebf8b8288782610ec5
Robert B. Fesko <fesko.robert@gmail.com>
2023-11-12 17:24:07 +0000

    update

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,9 +2,3 @@ sudo apt update -q
 sudo apt upgrade -y -q
 sudo apt-get install libx11-dev -y
 sudo apt install maven -y
-export PATH_TO_FX=/workspaces/hse-swt-timetracking/libs/javafx-sdk-21.0.1/lib
-sudo apt-get install -y xvfb
-sudo apt-get install -y libgtk-3-0 libglu1-mesa libxi6 libgconf-2-4
-sudo chmod 1777 /tmp/.X11-unix
-Xvfb :99 &
-export DISPLAY=:99
